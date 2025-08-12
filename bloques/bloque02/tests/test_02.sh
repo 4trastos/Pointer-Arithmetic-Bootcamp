b8866664d40b19738fba5a258007998fef278ed2
4trastos <93524280+4trastos@users.noreply.github.com>
2025-08-12 08:14:50 +0200

    Update test_02.sh

--- bloques/bloque02/tests/test_02.sh
+++ bloques/bloque02/tests/test_02.sh
@@ -2,10 +2,10 @@
 set -e
 "./ejercicios/ejercicio_02.out"
 output=$(./ejercicios/ejercicio_02.out)
-if [ "$output" = "1 2 3 4 5" ]; then
+if [ "$output" = "12345" ]; then
     echo "OK"
     exit 0
 else
-    echo "FALLO: salida='$output' (esperado: 1 2 3 4 5)"
+    echo "FALLO: salida='$output' (esperado: 12345)"
     exit 1
 fi
